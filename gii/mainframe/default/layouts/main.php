08dbd25f5f082ee938eaeeb2f3efddee0637f25b
Klimov Paul <klimov.paul@gmail.com>
2015-08-14 13:22:43 +0300

    'MainFrame' Gii generator created

---
+++ gii/mainframe/default/layouts/main.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This is the template for generating the main layout view file.
+ */
+
+/* @var $this yii\web\View */
+/* @var $generator yii2tech\admin\gii\mainframe\Generator */
+
+echo "<?php\n";
+?>
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use yii\bootstrap\Html;
+use yii2tech\admin\widgets\ContextLinks;
+<?= "?>" ?>
+<?= "<?php " ?> $this->beginContent($this->findViewFile('/layouts/overall')); ?>
+<div class="<?= "<?= " ?>str_replace('/', '-', $this->context->action->getUniqueId()) ?>">
+    <h1><?= "<?= " ?> Html::encode(isset($this->params['header']) ? $this->params['header'] : $this->title) ?></h1>
+
+    <p>
+        <?= "<?= " ?> ContextLinks::widget() ?>
+    </p>
+
+    <?= "<?= " ?> $content ?>
+</div>
+<?= "<?php " ?> $this->endContent(); ?>
